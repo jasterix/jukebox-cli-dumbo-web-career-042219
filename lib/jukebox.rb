61133640bf14f6b0124fea939f5093196d74b907
Jasterix C <loune.calixte@gmail.com>
2019-04-16 06:06:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,5 +19,9 @@ songs = [
 # puts say_hello(username)
 
 def helper_method
-  puts 
+  puts (I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program)
 end
