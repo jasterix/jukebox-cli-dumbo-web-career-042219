c0f8fc937834b2ba7f980e015e6e4fbfacb7b4a0
Jasterix C <loune.calixte@gmail.com>
2019-04-16 06:20:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,11 +19,11 @@ songs = [
 # puts say_hello(username)
 
 def helper_method
-  puts (I accept the following commands:
-  puts - help : displays this help message
-  puts - list : displays a list of songs you can play
-  puts - play : lets you choose a song to play
-  puts - exit : exits this program)
+  puts "(I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program)"
 end
 
 def list(songs)
